8aa999f56f581167c71417e67dec1649032bd78e
Acezio <bolide2005@163.com>
2016-05-01 23:01:28 +0800

    author:acezio.han msg:python spider 0.3 demo

--- socket_pro.py
+++ socket_pro.py
@@ -1,6 +1,16 @@
 import urllib2
+import urllib
 
-req = urllib2.Request("http://www.baidu.com")
-rep = urllib2.urlopen(req)
-content = rep.read()
-print content
+
+url = 'http://www.baidu.com/cgi-bin/register.cgi'
+
+user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+values = {'name' : 'WHY',
+          'location' : 'SDU',
+          'language' : 'Python' }
+
+headers = { 'User-Agent' : user_agent }
+data = urllib.urlencode(values)
+req = urllib2.Request(url, data, headers)
+response = urllib2.urlopen(req)
+the_page = response.read()
