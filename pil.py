996e48788869f74d00a2ebda4763bd0e6acadc42
Acezio <bolide2005@163.com>
2016-05-10 22:41:33 +0800

    modify pil

--- pil.py
+++ pil.py
@@ -34,3 +34,30 @@ for t in range(4):
 # 模糊:
 image = image.filter(ImageFilter.BLUR)
 image.save('code.jpg', 'jpeg');
+
+from urllib2 import Request, urlopen, URLError, HTTPError
+
+req = Request('http://bbs.csdn.net/callmewhy')
+  
+try:  
+  
+    response = urlopen(req)  
+  
+except URLError, e:  
+
+    if hasattr(e, 'code'):  
+  
+        print 'The server couldn\'t fulfill the request.'  
+  
+        print 'Error code: ', e.code  
+
+    elif hasattr(e, 'reason'):  
+  
+        print 'We failed to reach a server.'  
+  
+        print 'Reason: ', e.reason  
+  
+  
+else:  
+    print 'No exception was raised.'  
+    # everything is fine  
