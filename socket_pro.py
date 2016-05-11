b1128263a351e59195c1b630d9ce9afd8ae7cf05
Acezio <bolide2005@163.com>
2016-05-11 23:34:49 +0800

    modify socket

--- socket_pro.py
+++ socket_pro.py
@@ -1,16 +1,63 @@
-import urllib2
-import urllib
+__author__ = '绍文'
 
 
-url = 'http://www.baidu.com/cgi-bin/register.cgi'
+import Image, ImageDraw, ImageFont, ImageFilter
+import random
 
-user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
-values = {'name' : 'WHY',
-          'location' : 'SDU',
-          'language' : 'Python' }
+# 随机字母:
+def rndChar():
+    return chr(random.randint(65, 90))
 
-headers = { 'User-Agent' : user_agent }
-data = urllib.urlencode(values)
-req = urllib2.Request(url, data, headers)
-response = urllib2.urlopen(req)
-the_page = response.read()
+# 随机颜色1:
+def rndColor():
+    return (random.randint(64, 255), random.randint(64, 255), random.randint(64, 255))
+
+# 随机颜色2:
+def rndColor2():
+    return (random.randint(32, 127), random.randint(32, 127), random.randint(32, 127))
+
+# 240 x 60:
+width = 60 * 4
+height = 60
+image = Image.new('RGB', (width, height), (255, 255, 255))
+# 创建Font对象:
+font = ImageFont.truetype('Arial.ttf', 36)
+# 创建Draw对象:
+draw = ImageDraw.Draw(image)
+# 填充每个像素:
+for x in range(width):
+    for y in range(height):
+        draw.point((x, y), fill=rndColor())
+# 输出文字:
+for t in range(4):
+    draw.text((60 * t + 10, 10), rndChar(), font=font, fill=rndColor2())
+# 模糊:
+image = image.filter(ImageFilter.BLUR)
+image.save('code.jpg', 'jpeg');
+
+from urllib2 import Request, urlopen, URLError, HTTPError
+
+req = Request('http://bbs.csdn.net/callmewhy')
+  
+try:  
+  
+    response = urlopen(req)  
+  
+except URLError, e:  
+
+    if hasattr(e, 'code'):  
+  
+        print 'The server couldn\'t fulfill the request.'  
+  
+        print 'Error code: ', e.code  
+
+    elif hasattr(e, 'reason'):  
+  
+        print 'We failed to reach a server.'  
+  
+        print 'Reason: ', e.reason  
+  
+  
+else:  
+    print 'No exception was raised.'  
+    # everything is fine  
